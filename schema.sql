0f5f0f5c383239b96f5108702a955d8cda0fe0ed
вашÐДмитрий Кречетников <dkrech07@gmail.com>
2021-12-10 11:08:32 +0500

    ÐСохраняет sql-файлы, дорабатывает басхему

--- schema.sql
+++ schema.sql
@@ -6,38 +6,41 @@ USE taskforce;
 
 CREATE TABLE categories (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    category VARCHAR(128) NOT NULL
-);
-
-CREATE TABLE locations (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(128) NOT NULL,
-    coords VARCHAR(128) NOT NULL
+    icon VARCHAR(128) NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE cities (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    city VARCHAR(128) NOT NULL,
+    lat VARCHAR(128) NOT NULL,
+    long VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE profiles (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    bd INT UNSIGNED NOT NULL,
+    about VARCHAR(128) NOT NULL,
+    phone VARCHAR(128) NOT NULL,
+    skype VARCHAR(128) NOT NULL,
     role INT UNSIGNED NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(128) NOT NULL,
-    description VARCHAR(128) NOT NULL,
     name VARCHAR(128) NOT NULL,
-    birthday INT UNSIGNED NOT NULL,
-    location_id INT UNSIGNED NOT NULL,
+    city_id INT UNSIGNED NOT NULL,
     average_rating INT UNSIGNED NOT NULL,
     avatar_link VARCHAR(128) NOT NULL,
-    phone VARCHAR(128) NOT NULL,
-    skype VARCHAR(128) NOT NULL,
     messanger VARCHAR(128) NOT NULL,
     password CHAR(64) NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES locations(id),
+    FOREIGN KEY (city_id) REFERENCES cities(id),
     UNIQUE INDEX email(email)
 );
 
 CREATE TABLE users_specializations (
     user_id INT UNSIGNED NOT NULL,
     specialization_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES profiles(id),
     FOREIGN KEY (specialization_id) REFERENCES categories(id)
 );
 
@@ -50,26 +53,26 @@ CREATE TABLE tasks (
     name VARCHAR(128) NOT NULL,
     description VARCHAR(128) NOT NULL,
     category_id INT UNSIGNED NOT NULL,
-    location_id INT UNSIGNED NOT NULL,
+    city_id INT UNSIGNED NOT NULL,
     price INT UNSIGNED NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deadline TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     file_link VARCHAR(128),
-    FOREIGN KEY (customer_id) REFERENCES users(id),
-    FOREIGN KEY (executor_id) REFERENCES users(id),
+    FOREIGN KEY (customer_id) REFERENCES profiles(id),
+    FOREIGN KEY (executor_id) REFERENCES profiles(id),
     FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (location_id) REFERENCES locations(id)
+    FOREIGN KEY (city_id) REFERENCES cities(id)
 );
 
-CREATE TABLE reviews (
+CREATE TABLE opinions (
+    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    rate INT UNSIGNED NOT NULL,
+    description VARCHAR(128) NOT NULL,
     customer_id INT UNSIGNED NOT NULL,
     executor_id INT UNSIGNED NOT NULL,
     task_id INT UNSIGNED NOT NULL,
-    rating INT UNSIGNED NOT NULL,
-    review VARCHAR(128) NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES users(id),
-    FOREIGN KEY (executor_id) REFERENCES users(id),
+    FOREIGN KEY (customer_id) REFERENCES profiles(id),
+    FOREIGN KEY (executor_id) REFERENCES profiles(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
@@ -79,6 +82,6 @@ CREATE TABLE responses (
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     comment VARCHAR(128) NOT NULL,
     price INT UNSIGNED NOT NULL,
-    FOREIGN KEY (executor_id) REFERENCES users(id),
+    FOREIGN KEY (executor_id) REFERENCES profiles(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
