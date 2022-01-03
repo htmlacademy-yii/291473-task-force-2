c56f8c060feb589ab2981867a902fd177e2537dc
Дмитрий <dkrech07@gmail.com>
2022-01-03 06:30:07 +0500

    Добавляет файл подключения к БД в исключения

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=taskforce',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
