ad84f4dba50273be95340882cbb0ad06f22ab1a7
Дмитрий <dkrech07@gmail.com>
2022-01-14 01:56:57 +0500

    Начинает выводить данные на страницу заданий

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -5,21 +5,12 @@ namespace app\controllers;
 use app\models\Tasks;
 use yii\web\Controller;
 
-
-use app\models\Contact;
-use Yii;
-use yii\db\Query;
-use yii\web\NotFoundHttpException;
-use yii\web\Response;
-
-
-
-
 class TasksController extends Controller
 {
     public function actionIndex()
     {
         $query = Tasks::find()
+            // ->joinWith('categories')
             ->where(['status' => NULL])
             ->orderBy('dt_add DESC');
 
--- views/tasks/index.php
+++ views/tasks/index.php
@@ -1,53 +1,30 @@
 <?php
-print_r($tasks);
+
+use yii\helpers\Html;
+
+print_r($tasks[0]);
 ?>
+
 <div class="left-column">
     <h3 class="head-main head-task">Новые задания</h3>
-    <div class="task-card">
-        <div class="header-task">
-            <a href="#" class="link link--block link--big">Перевести войну и мир на клингонский</a>
-            <p class="price price--task">3400 ₽</p>
+
+    <?php foreach ($tasks as $task) : ?>
+        <div class="task-card">
+            <div class="header-task">
+                <a href="#" class="link link--block link--big"><?= Html::encode($task->name) ?></a>
+                <p class="price price--task">3400 ₽</p>
+            </div>
+            <p class="info-text"><span class="current-time">4 часа </span>назад</p>
+            <p class="task-text"><?= Html::encode($task->description) ?></p>
+            <div class="footer-task">
+                <p class="info-text town-text">Санкт-Петербург, Центральный район</p>
+                <p class="info-text category-text">Переводы</p>
+                <a href="#" class="button button--black">Смотреть Задание</a>
+            </div>
         </div>
-        <p class="info-text"><span class="current-time">4 часа </span>назад</p>
-        <p class="task-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor
-            nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie. Mauris malesuada nisi sit amet augue accumsan tincidunt.
-        </p>
-        <div class="footer-task">
-            <p class="info-text town-text">Санкт-Петербург, Центральный район</p>
-            <p class="info-text category-text">Переводы</p>
-            <a href="#" class="button button--black">Смотреть Задание</a>
-        </div>
-    </div>
-    <div class="task-card">
-        <div class="header-task">
-            <a href="#" class="link link--block link--big">Перевести войну и мир на клингонский</a>
-            <p class="price price--task">3400 ₽</p>
-        </div>
-        <p class="info-text"><span class="current-time">4 часа </span>назад</p>
-        <p class="task-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor
-            nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie. Mauris malesuada nisi sit amet augue accumsan tincidunt.
-        </p>
-        <div class="footer-task">
-            <p class="info-text town-text">Санкт-Петербург, Центральный район</p>
-            <p class="info-text category-text">Переводы</p>
-            <a href="#" class="button button--black">Смотреть Задание</a>
-        </div>
-    </div>
-    <div class="task-card">
-        <div class="header-task">
-            <a href="#" class="link link--block link--big">Перевести войну и мир на клингонский</a>
-            <p class="price price--task">3400 ₽</p>
-        </div>
-        <p class="info-text"><span class="current-time">4 часа </span>назад</p>
-        <p class="task-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor
-            nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie. Mauris malesuada nisi sit amet augue accumsan tincidunt.
-        </p>
-        <div class="footer-task">
-            <p class="info-text town-text">Санкт-Петербург, Центральный район</p>
-            <p class="info-text category-text">Переводы</p>
-            <a href="#" class="button button--black">Смотреть Задание</a>
-        </div>
-    </div>
+    <?php endforeach; ?>
+
+
     <div class="pagination-wrapper">
         <ul class="pagination-list">
             <li class="pagination-item mark">
